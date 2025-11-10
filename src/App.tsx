306ff7557fd1887282c414e18dcf3a7e05392cf9
priyanjan <priyanjanjb@gmail.com>
2025-11-10 16:08:14 +0530

    feat: setup Vite + React project with TailwindCSS and add Text component with arrow SVG
    - Initialized project using Vite with React template
    - Installed and configured TailwindCSS for styling
    - Created reusable Text component for UI consistency
    - Added arrow SVG asset for navigation or decorative use

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import TextArea from './components/textArea'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My App</h1>
+      <TextArea />
+      <TextArea />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/textArea.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function textArea() {
+  return (
+    <div>
+      <textarea className='textArea' placeholder='TEXT' />
+    </div>
+  )
+}
+
+export default textArea
